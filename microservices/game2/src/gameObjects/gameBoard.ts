a91b3c26286f85e1733a67165585b2f735b5c648
trebours <tomarebours3@gmail.com>
2025-08-05 11:13:33 +0200

    modif game board

--- microservices/game2/src/gameObjects/gameBoard.ts
+++ microservices/game2/src/gameObjects/gameBoard.ts
@@ -4,6 +4,9 @@ import * as math from "mathjs";
 export class gameBoard {
 
     private drawPile: Card[] = [];
+    private playerOne: Card[] = [];
+    private playerTwo: Card[] = [];
+    private discardPile: Card[] = [];
 
     constructor() {
         this.shufflingCards();
@@ -30,8 +33,45 @@ export class gameBoard {
         }
     }
 
+    public drawCard(player :number) {
+        if (this.drawPile.length <= 0)
+            return ;
+
+        if (player == 1) {
+            this.playerOne.push(this.drawPile[0]);
+            this.drawPile.splice(0, 1);
+        }
+        else if (player == 2) {
+            this.playerTwo.push(this.drawPile[0]);
+            this.drawPile.splice(0, 1);
+        }
+    }
+
+    public discardCard(player :number, cardId :number, cardNumber :number) {
+        if (this.playerOne.length <= 0 && this.playerTwo.length <= 0)
+            return ;
+        if (player == 1) {
+            for (var i = 0; i < this.playerOne.length; i++) {
+                if (this.playerOne[i].getType() == cardId && this.playerOne[i].getNumber() == cardNumber) {
+                    this.discardPile.push(this.playerOne[i]);
+                    this.playerOne.splice(i, 1);
+                    break;
+                }
+            }
+        }
+        if (player == 2) {
+            for (var i = 0; i < this.playerTwo.length; i++) {
+                if (this.playerTwo[i].getType() == cardId && this.playerTwo[i].getNumber() == cardNumber) {
+                    this.discardPile.push(this.playerTwo[i]);
+                    this.playerTwo.splice(i, 1);
+                    break;
+                }
+            }
+        }
+    }
+
     public getCard(id:number):number {
 
-        return ((this.drawPile[id].getType()*100) + this.drawPile[id].getNumber()) as number;
+        return ((this.drawPile[id].getType()) + (this.drawPile[id].getNumber() % 100)) as number;
     }
 }
